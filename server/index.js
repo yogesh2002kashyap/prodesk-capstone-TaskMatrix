ae7d4a4d6677d41f80107d39437fd6db758b8dff
Yogesh <yogesh2003kashyap@gmail.com>
2026-07-06 22:33:22 +0530

    chore: replace startup console.log with console.info, complete production log sweep

--- server/index.js
+++ server/index.js
@@ -83,9 +83,9 @@ if (process.env.NODE_ENV !== 'test') {
   mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
-      console.log('MongoDB connected');
+      console.info('MongoDB connected');
       app.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
+        console.info(`Server running on port ${process.env.PORT}`);
       });
     })
     .catch((err) => console.error('MongoDB connection error:', err));
