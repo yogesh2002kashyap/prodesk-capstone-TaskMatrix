5bcb367e7e773e04278f6613659378d57dd8454e
Yogesh <yogesh2003kashyap@gmail.com>
2026-07-07 11:26:31 +0530

    fix: remove parameter validation middleware from session route

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-
 const {
     createCheckoutSession,
     getSession,
 } = require('../controllers/stripeController');
-const { validateParams } = require('../middleware/validate');
-const { sessionParamSchema } = require('../validators/schemas');
 
 router.post('/create-checkout-session',  createCheckoutSession);
 
 router.get(
     '/session/:sessionId',
-    validateParams(sessionParamSchema),
     getSession
 );
 
