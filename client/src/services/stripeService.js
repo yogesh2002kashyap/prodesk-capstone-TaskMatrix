4c5ca5f622588a633c07b4972b49c850f402dafa
Yogesh <yogesh2003kashyap@gmail.com>
2026-07-12 18:16:35 +0530

    fix: correct data path in checkout session response handling

--- client/src/services/stripeService.js
+++ client/src/services/stripeService.js
@@ -2,9 +2,9 @@ import api from './api';
 
 export const createCheckoutSession = async () => {
     const r = await api.post('/stripe/create-checkout-session');
-    return r.data.url;
+    return r.data.data.url;
 };
 export const getCheckoutSession = async (sessionId) => {
     const res = await api.get(`/stripe/session/${sessionId}`);
-    return res.data;
+    return res.data.data;
 }
